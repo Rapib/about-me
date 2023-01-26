3e239072671f912f189efb10fb699897eb20d605
Thomas Lau <tomslau@outlook.com>
2023-01-25 17:14:29 -0800

    lab03

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 'use strict';
+let score = 0;
 
 let myName = prompt('Is my name Thomas? Please type yes or no').toLowerCase();
 
 if (myName === 'yes' || myName === 'y') {
   // console.log("Is my name Thomas? "+ myName);
   alert('Correct!');
+  score++;
 } else if (myName === 'no' || myName === 'n') {
   // console.log("Is my name Thomas? "+ myName);
   alert('Wrong!');
@@ -18,6 +20,7 @@ let likeMovies = prompt('Do I like movies? Please type y or n').toLowerCase();
 if (likeMovies === 'y' || likeMovies === 'yes') {
   // console.log("Do I like movies? " + likeMovies);
   alert('You are right!');
+  score++;
 } else if (likeMovies === 'n' || likeMovies === 'no') {
   // console.log("Do I like movies? " + likeMovies);
   alert('Sorry, wrong guess');
@@ -31,6 +34,7 @@ let degree = prompt('Do I have a degree in film? Please answer YES or NO').toUpp
 if (degree === 'YES' || degree === 'Y') {
   // console.log("Do I have a degree in film? " + degree);
   alert('Yes!');
+  score++;
 } else if (degree === 'NO' || degree === 'N') {
   // console.log("Do I have a degree in film? " + degree);
   alert('I do have a degree in film.');
@@ -44,9 +48,10 @@ let math = prompt('Yes or no question: Am I good at math?').toUpperCase();
 if (math === 'YES' || math === 'Y') {
   // console.log("Am I good at math? " + math);
   alert('It\'s ok!');
+  score++;
 } else if (math === 'NO' || math === 'N') {
   // console.log("Am I good at math? " + math);
-  alert('About average.');
+  alert('no.');
 } else {
   // console.log("Am I good at math? " + math);
   alert('-_-');
@@ -56,7 +61,8 @@ let code = prompt('Do I like to code? Please answer yes or no.').toLowerCase();
 
 if (code === 'yes' || code === 'y') {
   // console.log("Do I like to code? " + code);
-  alert('You are 50% right!');
+  alert('You are 60% right!');
+  score++;
 } else if (code === 'no' || code === 'n') {
   // console.log("Do I like to code? " + code);
   alert('You are 50% right!');
@@ -73,22 +79,94 @@ alert('Hi, ' + greeting + ' !');
 
 let guessNoAttempts = 3;
 let guessNo = prompt('Guess a number between 1 to 10.');
-while (guessNo !== '3' && guessNoAttempts > 0) {
-  
-  console.log('not 3');
-  if (guessNo < 3 && guessNo > 0) {
-    console.log('too low');
-    alert('too low');
-  } else if (guessNo > 3 && guessNo <= 10) {
-    console.log('too high');
-    alert('too high');
-  } else {
-    console.log(guessNo);
-    alert('please enter a number between 1 to 10');
-  }
-  guessNoAttempts--;
-  alert(`You have ${guessNoAttempts + 1} attempt left!`);
-  guessNo = prompt('Guess a number between 1 to 10.');
-}
-alert('the answer is 3');
+if (guessNo === 3) {
+  score++;
+  alert('correct');
+} else {
+  while (guessNo !== '3' && guessNoAttempts > 0) {
 
+    console.log('not 3');
+    if (guessNo < 3 && guessNo > 0) {
+      console.log('too low');
+      alert('too low');
+    } else if (guessNo > 3 && guessNo <= 10) {
+      console.log('too high');
+      alert('too high');
+    } else {
+      console.log(guessNo);
+      alert('please enter a number between 1 to 10');
+    }
+    guessNoAttempts--;
+    alert(`You have ${guessNoAttempts + 1} attempt left!`);
+    guessNo = prompt('Guess a number between 1 to 10.');
+  }
+  alert('the answer is 3');
+}
+
+// Question 7 array
+
+let color = ['green', 'orange', 'red', 'blue'];
+let guessColor = prompt('What color do I like?');
+let guessColorAttempts = 6;
+let userCorrect = false;
+
+// need to user input in color array
+while (guessColorAttempts) {
+  guessColorAttempts--;
+  alert(`you have ${guessColorAttempts} attempts left.`);
+
+  for (let i = 0; i < color.length; i++) {
+    if (guessColor === color[i]) {
+      userCorrect = true;
+    }
+  }
+  if (userCorrect === true) {
+    score++;
+    userCorrect = false;
+    alert("correct");
+    break;
+  } else {
+    alert("sorry, guess again");
+    guessColor = prompt('What color do I like?');
+  }
+
+}
+alert(`answers can be ${color} & your final score is ${score}!`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < color.length; i++) {
+//   if (color[i] === guessColor) {
+//     alert('correct');
+//     console.log(color[i]);
+//     score++;
+//     console.log(`final score: ${score}!`);
+//     alert(`final score: ${score}!`);
+//   }
+//   else {
+//     while (guessColorAttempts > 0 && color[i] !== guessColor) {
+//       guessColorAttempts--;
+//       console.log(guessColorAttempts);
+
+//       alert(`You have ${guessColorAttempts +1} attempt left!`);
+//       guessColor = prompt('What color do I like?');
+
+
+//     }
+//     console.log(`final score: ${score}!`);
+//     console.log(`the answers are ${color}`);
+//     alert(`the answers are ${color}`);
+//   }
+// }
